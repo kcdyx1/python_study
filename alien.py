606489cf52d4cc9dff9bbc8984519a49e6eec85e
kcdyx1 <kcdyx1@hotmail.com>
2017-12-12 09:51:20 +0800

    change

--- alien.py
+++ alien.py
@@ -1,2 +1,8 @@
 m = input("您好，请输入...")
 print(m)
+
+soup = bs4.BeautifulSoup(res.text, "html.parser")
+print(soup)
+targets = soup.find_all("div", class_="hd")
+for each in targets:
+	print(each.a.span.text)
