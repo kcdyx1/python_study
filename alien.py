424a45384acff2f3133fff4ef0dfb256a63f304e
kcdyx1 <kcdyx1@hotmail.com>
2017-12-24 15:29:47 +0800

    new python code

--- alien.py
+++ alien.py
@@ -1,8 +1,3 @@
 m = input("您好，请输入...")
 print(m)
 
-soup = bs4.BeautifulSoup(res.text, "html.parser")
-print(soup)
-targets = soup.find_all("div", class_="hd")
-for each in targets:
-	print(each.a.span.text)
