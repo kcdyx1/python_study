e62ea6c554c313ccca5fc0e2f7ba5e5c681e4ac3
kcdyx1 <kcdyx1@hotmail.com>
2018-01-12 15:44:00 +0800

    有道理

--- globaltechmap/spider/fenci_chuli.py
+++ globaltechmap/spider/fenci_chuli.py
@@ -14,11 +14,24 @@ with open("/Users/kangchen/python_study/globaltechmap/spider/extra_dict/stop_wor
         if not word:
             break
 
-field = 'xincailiao'
-year = '2015'
+fields ={
+        'hy':'haiyang',
+        'hk': 'hangkong',
+        'ht': 'hangtian',
+        'kjzl':'kejizhanlue',
+        'ny': 'nengyuan',
+        'sw': 'shengwu',
+        'xjzz': 'xianjinzhizao',
+        'xcl':'xincailiao',
+        'xx': 'xinxi'
+}
+
+field_alia = input("请输入需要分析的领域首字母：")
+field = fields[field_alia]
+year = input("请输入需要分析的年份：")
 
 # 输入需要分析的源文件
-daifenxi = field + '_neirong_' + year + '.csv'
+daifenxi = "/Users/kangchen/python_study/globaltechmap/spider/MySQL_Results/" + field + '_neirong_' + year + '.csv'
 fencijieguo = []
 stat = {}
 
@@ -35,7 +48,7 @@ with open(daifenxi, 'r') as fr:
 
 # 结果排序
 stat = sorted(stat.items(), key=lambda d: d[1], reverse=True)
-
+print("分析完成，结果正在保存……")
 # 结果保存
 jieguo_dizhi = "/Users/kangchen/python_study/globaltechmap/spider/Results/" 
 fencijieguo_wenjian = jieguo_dizhi + field + year +'.csv'
@@ -43,3 +56,4 @@ fencijieguo_wenjian = jieguo_dizhi + field + year +'.csv'
 with open(fencijieguo_wenjian, 'w') as fw:
     for item in stat:
         fw.write(item[0] + ',' + str(item[1]) + '\n')
+print("结果保存完毕，请放心食用！")
--- globaltechmap/spider/mysqltest.py
+++ globaltechmap/spider/mysqltest.py
@@ -4,10 +4,25 @@ import pymysql
 
 conn = pymysql.connect(host ='localhost', port =8889, user ='root', password ='root', db='globaltechmap', charset='utf8')
 cursor = conn.cursor()
-# rowNums = cursor.execute('SELECT news_id FROM xincailiao')  
-# print('查询的行数为' + str(rowNums)
-# conn.close()
-sql = "SELECT content FROM xincailiao where date between '2015-01-01' and '2015-12-31'"
+
+fields ={
+        'hy':'haiyang',
+        'hk': 'hangkong',
+        'ht': 'hangtian',
+        'kjzl':'kejizhanlue',
+        'ny': 'nengyuan',
+        'sw': 'shengwu',
+        'xjzz': 'xianjinzhizao',
+        'xcl':'xincailiao',
+        'xx': 'xinxi'
+}
+
+field_alia = input("请输入需要查询的领域首字母：")
+field = fields[field_alia]
+year_input = input("请输入需要查询的年份：")
+year = "'" + year_input
+
+sql = "SELECT content FROM "+ field + " where date between " + year + "-01-01' and " + year + "-12-31'"
 words = ()
 try:
     cursor.execute(sql)    #执行sql语句 
@@ -15,9 +30,12 @@ try:
     # print(len(results))
     for row in results:
         content = row[0]
-        with open('xincailiao_neirong_2015.csv', 'a') as fw:
+        # 保存查询结果
+
+        chaxuejieguo = "/Users/kangchen/python_study/globaltechmap/spider/MySQL_Results/" + field + "_neirong_" + year.replace("'","") + ".csv"
+        with open(chaxuejieguo, 'a') as fw:
                  fw.write(content)
-        print(content)
+    print("中共中央贺电：查询结果已经成功保存！")
 except Exception as e:
     raise e
 finally:
